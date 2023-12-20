cd3f50b002e5e55dbc9ac474d34063058819be79
xzx <2747668191@qq.com>
2023-12-20 11:05:23 +0800

    删除

--- python练习/课堂/1213/djangoProject/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from Book.models import Book
-
-#查询所以数据
-class Bookserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book #同models中BooK包
-        fields = '__all__'
