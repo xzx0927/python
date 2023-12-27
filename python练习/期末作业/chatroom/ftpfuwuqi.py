6147118c88f56a79bc7643f47fb22aa3d659140d
xzx <2747668191@qq.com>
2023-12-27 13:16:10 +0800

    修改

--- python练习/期末作业/chatroom/ftpfuwuqi.py
+++ python练习/期末作业/chatroom/ftpfuwuqi.py
@@ -1,22 +1,45 @@
 import socket
 import threading
+import time
 
 import xlwt
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('10.12.220.172', 211))
+print("等待连接")
 
-def handler_jiesho(conn):
+s.listen()
+user_list = []
+socket_list = []  # 用于储存ip和其他信息
+
+
+def donglutishi(s, username):
+    # try:
+    print(s.recv(1024).decode('utf-8'))
+    return s.recv(1024).decode('utf-8')
+    # except:
+    #     for _conn in socket_list:
+    #         _conn.send(('系统消息' + username + '离开了聊天室').encode('utf-8'))
+    #         user_list.remove(username)
+    #         s.close()
+
+
+def he(s, username):  # 接受消息并转发
+    if username.endswith('~'):  # 判断是否为用户名
+        kehuduan = socket_list.copy()
+        for _conn in kehuduan:
+            for _user in user_list:
+                _conn.send(_user.encode('utf-8'))
     while True:
-        bt = conn.recv(1024)
-        msg = bt.decode('utf-8')
-        print("客户端IP：" + conn.getpeername()[0] + "端口：" + str(conn.getpeername()[1]) + "发送的是" + msg)
-        # _conn.getpeername()返回的是addr（IP和端口）
-        for _conn in connList:
-            if conn != _conn:
-                _conn.send(
-                    ('ip:' + conn.getpeername()[0] + "端口:" + str(conn.getpeername()[1]) + '内容是:' + msg).encode(
-                        'utf-8'))
-        if msg == ".":
+        xiaoxi = s.recv(1024).decode('utf-8')
+        print(xiaoxi)
+        if xiaoxi == '':
             break
+        else:
+            print(socket_list)
+            for _conn in socket_list:
+                _conn.send(('用户' + username + '说:' + xiaoxi).encode('utf-8'))
+
 
 def chushihuan():
     wb = xlwt.Workbook()
@@ -29,19 +52,19 @@ def chushihuan():
     print("初始化用户成功")
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('10.12.220.172', 211))
-print("等待连接")
-chushihuan()
-s.listen(5)
-global connList
-connList = []
-conn, addr = s.accept()
-list.append(conn)
-print("连接成功")
-bt = conn.recv(1024)
-tp = threading.Thread(target=handler_jiesho, args=(conn,))
-tp.start()
-# msg = bt.decode('utf-8')  # 将接受的消息解码
-# print(msg)
-# conn.send(msg.encode('utf-8'))  # 转发并编码
+chushihuan()  # 初始化用户信息
+while True:
+    conn, addr = s.accept()
+    socket_list.append(conn)
+    print("连接成功")
+    bt = conn.recv(1024).decode('utf-8')  # 接受用户名
+    bt = bt + '~'
+    user_list.append(bt)
+    user_list = list(set(user_list))  # 去重
+    for _conn in socket_list:
+        x = str(bt).strip('~')
+        _conn.send(('系统消息:' + x + '进入聊天室').encode('utf-8'))
+    print(user_list)
+    time.sleep(1)
+    tp = threading.Thread(target=he, args=(conn, bt,))
+    tp.start()
